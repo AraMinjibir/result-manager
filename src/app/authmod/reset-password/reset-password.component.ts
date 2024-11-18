cf5c22596cf8e212fe0d106974036f6d99a5edae
AraMjb <abubakar.aramjb@gmail.com>
2024-11-18 11:19:47 +0100

    final submission

--- src/app/authmod/reset-password/reset-password.component.ts
+++ src/app/authmod/reset-password/reset-password.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+
 
 @Component({
-  selector: 'app-reset-password',
+  selector: 'reset-password',
   standalone: true,
-  imports: [],
+  imports: [CardModule, InputTextModule,
+    FloatLabelModule, ButtonModule
+  ],
   templateUrl: './reset-password.component.html',
   styleUrl: './reset-password.component.scss'
 })
