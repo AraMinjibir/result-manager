0e9ff1d31ed4f283993fa9a92bab64eba51c782e
Jabir Minjibir <jabir.minjibir@gmail.com>
2024-12-02 18:05:17 -0500

    Upgrade to angular version and remove depricated style constructs.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,23 @@
 import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'auth',
+    redirectTo: 'home',
     pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent
   },
   {
     path: 'auth',
     loadChildren: () =>
       import('./authmod/authmod.module').then((a) => a.AuthmodModule),
   },
-
-  // This route should always be the last one.
-  {
+  {// This route should always be the last one.
     path: "**",
     component: PageNotFoundComponent
   }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-
+import { ButtonModule } from 'primeng/button';
+import { MenuModule } from 'primeng/menu';
+import { SidebarModule } from 'primeng/sidebar';
 
 @Component({
     selector: 'home',
-    imports: [],
+    imports: [ButtonModule, MenuModule, SidebarModule],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss'
 })
