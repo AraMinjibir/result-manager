049d349be32ce2b7a8c1a74a59badff644641a82
AraMjb <abubakar.aramjb@gmail.com>
2024-12-03 10:00:09 +0100

    final submission

--- src/app/app-layout/app-layout.component.ts
+++ src/app/app-layout/app-layout.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { SidebarModule } from 'primeng/sidebar';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [SidebarModule, ButtonModule],
+  imports: [SidebarModule, ButtonModule,
+    DashboardComponent
+  ],
   templateUrl: './app-layout.component.html',
   styleUrl: './app-layout.component.scss'
 })
