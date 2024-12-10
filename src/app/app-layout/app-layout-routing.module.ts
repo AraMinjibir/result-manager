f3a58650cda12b4c095afbf07239cc9416e98aca
AraMjb <abubakar.aramjb@gmail.com>
2024-12-10 17:10:01 +0100

    final submission

--- src/app/app-layout/app-layout-routing.module.ts
+++ src/app/app-layout/app-layout-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { StudentProfileComponent } from './student-profile/student-profile.component';
+import { ResultsEntryComponent } from './results-entry/results-entry.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'profile',
     component: StudentProfileComponent
+  },
+  {
+    path: 'results-entry',
+    component: ResultsEntryComponent
   }
 ]
 
