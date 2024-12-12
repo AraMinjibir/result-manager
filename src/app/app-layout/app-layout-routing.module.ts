78214c61594c0822a96c99329f2f7a909e25e889
AraMjb <abubakar.aramjb@gmail.com>
2024-12-12 11:31:02 +0100

    final submission

--- src/app/app-layout/app-layout-routing.module.ts
+++ src/app/app-layout/app-layout-routing.module.ts
@@ -5,6 +5,7 @@ import { StudentProfileComponent } from './student-profile/student-profile.compo
 import { ResultsEntryComponent } from './results-entry/results-entry.component';
 import { StudentRecordsComponent } from './student-records/student-records.component';
 import { ResultCompilationComponent } from './result-compilation/result-compilation.component';
+import { StudentsMarkRecodingComponent } from './students-mark-recoding/students-mark-recoding.component';
 
 
 
@@ -28,6 +29,10 @@ const routes: Routes = [
   {
     path: 'result-compilation',
     component: ResultCompilationComponent
+  },
+  {
+    path: 'marks-recording',
+    component: StudentsMarkRecodingComponent
   }
 ]
 
