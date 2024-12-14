3db36ee1f429c25cf77ec741d1aa11707ce7541f
AraMjb <abubakar.aramjb@gmail.com>
2024-12-14 21:12:05 +0100

    changes made

--- src/app/app-layout/app-layout.component.ts
+++ src/app/app-layout/app-layout.component.ts
@@ -2,21 +2,31 @@ import { Component } from '@angular/core';
 <<<<<<< HEAD
 import { ButtonModule } from 'primeng/button';
 import { SidebarModule } from 'primeng/sidebar';
+<<<<<<< HEAD
 =======
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
 >>>>>>> 02c99c17ae12fdd3a8d0f6026ee4a0acb1ea0ad6
+=======
+import { DashboardComponent } from './dashboard/dashboard.component';
+>>>>>>> 7f8cb53 (final submission)
 
 @Component({
   selector: 'app-layout',
   standalone: true,
 <<<<<<< HEAD
+<<<<<<< HEAD
   imports: [SidebarModule, ButtonModule],
 =======
   imports: [DashboardComponent, HeaderComponent, SidebarComponent],
 >>>>>>> 02c99c17ae12fdd3a8d0f6026ee4a0acb1ea0ad6
+=======
+  imports: [SidebarModule, ButtonModule,
+    DashboardComponent
+  ],
+>>>>>>> 7f8cb53 (final submission)
   templateUrl: './app-layout.component.html',
   styleUrl: './app-layout.component.scss'
 })
