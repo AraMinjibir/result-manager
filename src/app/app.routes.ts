bde0d79ee1660a7130bc4d08b228b25cb3006b2c
AraMjb <abubakar.aramjb@gmail.com>
2024-12-14 23:45:17 +0100

    final submission

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,12 @@
 import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
-  },
-  {
-    path: 'home',
-    component: HomeComponent
   },
   {
     path: 'auth',
