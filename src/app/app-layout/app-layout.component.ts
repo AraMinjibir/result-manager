3e4db0208a515750a3d909f00fb2556f6d8e0443
AraMjb <abubakar.aramjb@gmail.com>
2024-12-14 21:14:53 +0100

    changes made

--- src/app/app-layout/app-layout.component.ts
+++ src/app/app-layout/app-layout.component.ts
@@ -6,12 +6,16 @@ import { SidebarModule } from 'primeng/sidebar';
 =======
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeaderComponent } from './header/header.component';
+<<<<<<< HEAD
 import { SidebarComponent } from './sidebar/sidebar.component';
 
 >>>>>>> 02c99c17ae12fdd3a8d0f6026ee4a0acb1ea0ad6
 =======
 import { DashboardComponent } from './dashboard/dashboard.component';
 >>>>>>> 7f8cb53 (final submission)
+=======
+import { AvatarModule } from 'primeng/avatar';
+>>>>>>> e97d457 (final initial commit)
 
 @Component({
   selector: 'app-layout',
@@ -24,7 +28,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 >>>>>>> 02c99c17ae12fdd3a8d0f6026ee4a0acb1ea0ad6
 =======
   imports: [SidebarModule, ButtonModule,
-    DashboardComponent
+    DashboardComponent, HeaderComponent, AvatarModule
   ],
 >>>>>>> 7f8cb53 (final submission)
   templateUrl: './app-layout.component.html',
