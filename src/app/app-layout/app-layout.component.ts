98688b434e1db51b0f487aff0a628e35a3b85358
AraMjb <abubakar.aramjb@gmail.com>
2024-12-14 23:49:09 +0100

    sync remove branch

--- src/app/app-layout/app-layout.component.ts
+++ src/app/app-layout/app-layout.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
-import { ButtonModule } from 'primeng/button';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { AvatarModule } from 'primeng/avatar';
-
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [DashboardComponent, HeaderComponent, SidebarComponent, SidebarComponent, ButtonModule],
+
+  imports: [DashboardComponent, HeaderComponent, SidebarComponent],
   templateUrl: './app-layout.component.html',
   styleUrl: './app-layout.component.scss'
 })
