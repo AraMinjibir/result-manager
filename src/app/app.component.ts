3b13b816949b4a8c58881d6ce8d7018d9ddc03f5
AraMjb <abubakar.aramjb@gmail.com>
2024-12-14 21:06:12 +0100

    changes made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,11 @@ import { PrimeNGConfig } from 'primeng/api';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    imports: [RouterOutlet],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
   title = 'result-manager';
