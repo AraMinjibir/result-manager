546292e2a827d498a3b86e9d6c15967d8fc46b8d
AraMjb <abubakar.aramjb@gmail.com>
2024-12-16 14:27:30 +0100

    initial commit

---
+++ src/app/app-layout/notification-preference/notification-preference.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification-preference',
+  standalone: true,
+  imports: [],
+  templateUrl: './notification-preference.component.html',
+  styleUrl: './notification-preference.component.scss'
+})
+export class NotificationPreferenceComponent {
+
+}
