68a0f8095aac9318d7af301405c2ced949d6f841
AraMjb <abubakar.aramjb@gmail.com>
2024-12-17 13:58:10 +0100

    final submission

--- src/app/app-layout/notification-preference/notification-preference.component.ts
+++ src/app/app-layout/notification-preference/notification-preference.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { InputTextModule } from 'primeng/inputtext';
+import { NgFor } from '@angular/common';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-notification-preference',
   standalone: true,
-  imports: [],
+  imports: [CardModule, ReactiveFormsModule, ToggleButtonModule,
+            InputTextModule, NgFor, RadioButtonModule, ButtonModule],
   templateUrl: './notification-preference.component.html',
-  styleUrl: './notification-preference.component.scss'
+  styleUrls: ['./notification-preference.component.scss'],
 })
-export class NotificationPreferenceComponent {
+export class NotificationPreferenceComponent implements OnInit {
+  formGroup: FormGroup;
+  frequencyOptions = [
+    { value: 'monthly', label: 'Monthly' },
+    { value: 'weekly', label: 'Weekly' },
+    { value: 'daily', label: 'Daily' },
+  ];
 
+  ngOnInit() {
+    this.formGroup = new FormGroup({
+      email: new FormControl<boolean>(true),
+      emailAddress: new FormControl<string | null>(null),
+      sms: new FormControl<boolean>(true),
+      smsNotif: new FormControl<string | null>(null),
+      frequency: new FormControl<string>(null)
+    });
+  }
+  OnsubmitPreference(){
+    this.formGroup.reset();
+  }
 }
