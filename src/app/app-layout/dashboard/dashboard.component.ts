12b6532abc7d18b8bcb2652db1314a788f2fe534
AraMjb <abubakar.aramjb@gmail.com>
2024-12-24 14:33:02 +0100

    finall submission

--- src/app/app-layout/dashboard/dashboard.component.ts
+++ src/app/app-layout/dashboard/dashboard.component.ts
@@ -13,17 +13,5 @@ import { CardModule } from 'primeng/card';
 })
 export class DashboardComponent {
 
-  stats = [
-    {title: "Admitted Students", no: 100, },
-    {title: "Average Students", no: 50,},
-    {title: "Below Average", no: 30,},
-    {title: "Poor", no: 20,}
-  ];
-
-  recent = [
-    {title: "Assignments Given", no: 50},
-    {title: "C.A's Conducted", no: 3},
-    {title: "Quiz Performed", no: 5},
-    {title: "Debates Carried out", no: 2,}
-  ]
+  
 }
